64dede66dab6c1339e4ea51a99b03aa4dbbaf492
kyle <phantom-kilo@live.com>
2022-03-23 20:31:23 -0700

    Initial API call almost complete

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -3,13 +3,43 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 import Description from "./SubDocs/Description";
 import Results from "./SubDocs/Results";
 import SearchBar from "./SubDocs/SearchBar";
 
 const SearchPage = () => {
-  return <div>Search this shit</div>;
+  const [book, setBook] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchAuthor, setSearchAuthor] = useState("");
+  // const [wishlist, setWishlist] = useState([])
+  // const [selectedBook, setSelectedBook] = useState('')
+  const searchUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchName}+inauthor:${searchAuthor}`;
+
+  const makeApiCall = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    const results = await setBook(data);
+    return results;
+  };
+
+  const handleClick = async () => {
+    const call = await makeApiCall(searchUrl);
+    return call;
+  };
+
+  return (
+    <Container>
+      <Row>
+        <SearchBar
+          setSearchName={setSearchName}
+          setSearchAuthor={setSearchAuthor}
+          handleClick={handleClick}
+        />
+      </Row>
+    </Container>
+  );
 };
 
 export default SearchPage;
--- src/Components/SearchPage/SubDocs/SearchBar.js
+++ src/Components/SearchPage/SubDocs/SearchBar.js
@@ -1,9 +1,42 @@
-const SearchBar = () => {
-    return(
-        <div>
-            <h1>SearchBar</h1>
-        </div>
-    )
-}
+import { useRef } from "react";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-export default SearchBar
+const SearchBar = (props) => {
+  const setSearchAuthor = props.setSearchAuthor;
+  const setSearchName = props.setSearchName;
+  const handleClick = props.handleClick;
+  const authorRef = useRef();
+  const nameRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSearchName(nameRef.current.value);
+    setSearchAuthor(authorRef.current.value);
+    const call = await handleClick();
+    return call;
+  };
+
+  return (
+    <Card>
+      <Card.Title>Search Google Book API</Card.Title>
+      <Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Book Title</Form.Label>
+            <Form.Control type="text" ref={nameRef} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Book Author</Form.Label>
+            <Form.Control type="text" ref={authorRef} />
+          </Form.Group>
+          <Button type="submit">search</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default SearchBar;
